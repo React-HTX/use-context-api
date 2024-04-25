55b743ffd94aae4b5a0eb74597bbff6058943752
ZocaloHealth <developer@zocalo.health>
2024-04-25 17:44:44 -0500

    Updates.

--- components/MovieCountBadge/index.jsx
+++ components/MovieCountBadge/index.jsx
@@ -1,6 +1,6 @@
 const MovieCountBadge = ({ count }) => {
   return (
-    <span className="inline-block absolute -top-2 -right-2 text-center h-6 w-6 bg-white rounded-full text-blue-500">
+    <span className="inline-block absolute -top-2 -right-2 text-center h-6 w-6 bg-white rounded-full text-blue-500 border">
       {count}
     </span>
   );
